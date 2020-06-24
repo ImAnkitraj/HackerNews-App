0e3297ef81e1b2bb5c417e82a2d9023cecaace0f
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2020-06-24 12:22:06 +0530

    Initail commit

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,15 @@
+import 'package:news/src/resources/new_api_provider.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main(){
+  test('FetchTopIds returns a list of ids', (){
+    //setup of test case
+    // final sum = 1+3;
+  final newsApi = NewsApiProvider();
+    //expectation
+    // expect(sum, 4);
+  });
+}
